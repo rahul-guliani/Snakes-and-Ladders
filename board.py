c5b0fb5d2200866d703720d7ab5c123d00fc1344
khushwant-learn <irahulguliani@gmail.com>
2024-08-29 02:25:08 +0530

    ---

--- board.py
+++ board.py
@@ -114,7 +114,7 @@ def draw_board_with_ladders(ladders):
         print()     # To go to the next line   
 
 
-def draw_board_with_ladders_and_player(ladders, snakes, player_position):
+def draw_board_with_ladders_and_players(ladders, snakes, player_1_position, player_2_position):
     ladder_foots = [ladder[0] for ladder in ladders]    # first element of the each tuple in the list of tuples
     ladder_heads = [ladder[1] for ladder in ladders]    # second element ...
 
@@ -133,29 +133,34 @@ def draw_board_with_ladders_and_player(ladders, snakes, player_position):
                 (box_number not in ladder_heads) and \
                 (box_number not in snake_tails) and \
                 (box_number not in snake_heads) and \
-                (box_number != player_position):
-                print(str(box_number).rjust(4), end=' ')  # Add number in decending order (10 to 1) for alternative row starting from first row 
+                (box_number != player_1_position) and \
+                (box_number != player_2_position):
+                print(str(box_number).rjust(5), end=' ')  # Add number in decending order (10 to 1) for alternative row starting from first row 
 
             # Print ladder_foot with its index
             elif box_number in ladder_foots:
                 ladder_foot_index = ladder_foots.index(box_number)
-                print(f'H{ladder_foot_index + 1}'.rjust(4), end=' ')    # Adding 1 because index starts from zero
+                print(f'H{ladder_foot_index + 1}'.rjust(5), end=' ')    # Adding 1 because index starts from zero
 
             # Print ladder_head and ladder_head with player if they are at same position
             elif box_number in ladder_heads:
                 ladder_head_index = ladder_heads.index(box_number)
-                if box_number == player_position:
-                    print(f'H{ladder_head_index + 1}P'.rjust(4), end=' ')
+                if box_number == player_1_position:
+                    print(f'H{ladder_head_index + 1}P1'.rjust(5), end=' ')
+                elif box_number == player_2_position:
+                    print(f'H{ladder_head_index + 1}P2'.rjust(5), end=' ')
                 else:
-                    print(f'H{ladder_head_index + 1}'.rjust(4), end=' ')
+                    print(f'H{ladder_head_index + 1}'.rjust(5), end=' ')
 
             # Print snake_tail and snake_tail with player if they are at same position
             elif box_number in snake_tails:
                 snake_tail_index = snake_tails.index(box_number)
-                if box_number == player_position:
-                    print(f"S{snake_tail_index + 1}P".rjust(4), end=' ')
+                if box_number == player_1_position:
+                    print(f"S{snake_tail_index + 1}P1".rjust(5), end=' ')
+                elif box_number == player_2_position:
+                    print(f"S{snake_tail_index + 1}P2".rjust(5), end=' ')
                 else:
-                    print(f"S{snake_tail_index + 1}".rjust(4), end=' ')
+                    print(f"S{snake_tail_index + 1}".rjust(5), end=' ')
 
             # Print snake_head with its index
             # Why we are not checking the player's position for snake's head and ladder's foot
@@ -163,10 +168,14 @@ def draw_board_with_ladders_and_player(ladders, snakes, player_position):
             # keep player there
             elif box_number in snake_heads:
                 snake_head_index = snake_heads.index(box_number)
-                print(f"S{snake_head_index + 1}".rjust(4), end=' ')
+                print(f"S{snake_head_index + 1}".rjust(5), end=' ')
 
             # Print player if it is the only one on the box_number
+            elif box_number == player_1_position:
+                print(f'P1'.rjust(5), end=' ')
+
             else:
-                print(f'P'.rjust(4), end=' ')
+                print(f'P2'.rjust(5), end=' ')
+
 
         print()
--- game_stages.py
+++ game_stages.py
@@ -1,4 +1,4 @@
-from board import draw_board_with_ladders_and_player
+from board import draw_board_with_ladders_and_players
 from draw_ladders import draw_ladders_and_snakes
 import random
 
@@ -10,29 +10,50 @@ def game_start():
     snakes = snakes_and_ladders_dict['snakes']
     ladders = snakes_and_ladders_dict['ladders']
     
-    player_position = 0
-    while(True):
-        # Call the roll_dice_and_print_board() and receives the returned dictionary in a variable 
-        returned_dict = roll_dice_and_print_board(player_position, snakes, ladders)     # function executes 
-        play_interruption = returned_dict['play_interruption']
-        player_position = returned_dict['player_position']
-        
-        # Update the player_position
-        # player_position += final_number
+    player_1_position = 0
+    player_2_position = 0
 
-        if play_interruption:
+    while(True):
+        # Call the roll_dice() and receives the returned dictionary in a variable 
+
+        print(f"----------PLAYER_1-----------")
+        returned_dict_player_1 = roll_dice()     # function executes 
+        player_1_interruption = returned_dict_player_1['play_interruption']
+        if player_1_interruption:
             if (input('wanna exit game (y/n) ? :  ').lower()) == 'y':
                 break
-            
-            else:
-                play_interruption = False
-       
-        if player_position >= 100:
-            game_finished()
+        player_1_rolled_number = returned_dict_player_1['total_number_rolled']
+        player_1_position += player_1_rolled_number
+
+        # Draw Board
+        draw_board_with_ladders_and_players(snakes=snakes, ladders=ladders, player_1_position=player_1_position, player_2_position=player_2_position)
+
+        print(f"----------PLAYER_2-----------")
+        returned_dict_player_2 = roll_dice()
+        player_2_interruption = returned_dict_player_2['play_interruption']
+        if player_2_interruption:
+            if (input('wanna exit game (y/n) ? :  ').lower()) == 'y':
+                break
+        player_2_rolled_number = returned_dict_player_2['total_number_rolled']
+        player_2_position += player_2_rolled_number
+        
+        # Draw Board
+        draw_board_with_ladders_and_players(snakes=snakes, ladders=ladders, player_1_position=player_1_position, player_2_position=player_2_position)
+
+
+        # Update the player_position
+        # player_position += final_number
+    
+        if player_1_position >= 100:
+            game_finished('player_1')
             break
 
-def game_finished():
-    print('Player PPP finished the game.')
+        elif player_2_position >= 100:
+            game_finished('player_2')
+            break
+
+def game_finished(player):
+    print(f'Player {player} finished the game.')
 
 
 def play_game():
@@ -40,40 +61,39 @@ def play_game():
     if wanna_play_game.lower() == 'y':
         game_start()
 
-def roll_dice_and_print_board(player_position, snakes, ladders):
+def roll_dice():
 
-    ladder_foots = [ladder[0] for ladder in ladders]    # first element of the each tuple in the list of tuples
-    ladder_heads = [ladder[1] for ladder in ladders]    # second element ...
+    # ladder_foots = [ladder[0] for ladder in ladders]    # first element of the each tuple in the list of tuples
+    # ladder_heads = [ladder[1] for ladder in ladders]    # second element ...
 
-    snake_tails = [snake[0] for snake in snakes]
-    snake_heads = [snake[1] for snake in snakes]
+    # snake_tails = [snake[0] for snake in snakes]
+    # snake_heads = [snake[1] for snake in snakes]
 
     play_interruption = False
     roll_a_dice = input("Wanna roll the dice (r) ? :  ")
     if roll_a_dice.lower() == 'r':
         final_number = roll_a_dice_loop()
-        # global player_position 
-        player_position += final_number
-        print(f"Player PPP moves {final_number} {'place' if final_number == 1 else 'places'} forward to box-number: {player_position}")
+        # player_position += final_number
+        # print(f"{player} moves {final_number} {'place' if final_number == 1 else 'places'} forward to box-number: {player_position}")
 
     
         # Update Player position if there is a ladder foot on the current player position 
-        if player_position in ladder_foots:
-            player_position = ladder_heads[ladder_foots.index(player_position)]   # head at same index where foot is
-            print(f"your player is on a ladder's foot, happy to move it to its head that is box-number: {player_position}")
+        # if player_position in ladder_foots:
+        #     player_position = ladder_heads[ladder_foots.index(player_position)]   # head at same index where foot is
+        #     print(f"your player is on a ladder's foot, happy to move it to its head that is box-number: {player_position}")
 
-        if player_position in snake_heads:
-            player_position = snake_tails[snake_heads.index(player_position)]   # tail at same index where head is
-            print(f"Your player is on a snake's head, so sorry to take you to its tail that is box-number: {player_position}")
+        # if player_position in snake_heads:
+        #     player_position = snake_tails[snake_heads.index(player_position)]   # tail at same index where head is
+        #     print(f"Your player is on a snake's head, so sorry to take you to its tail that is box-number: {player_position}")
 
-        draw_board_with_ladders_and_player(ladders, snakes, player_position)    # Draw board with Player(P) on it at position 'player_position')
+        # draw_board_with_ladders_and_players(ladders, snakes, player_position)    # Draw board with Player(P) on it at position 'player_position')
 
 
     else:
         play_interruption = True     # If game interrupted by preesing any key other than 'r' or 'R'
         final_number = 0             # If game is interrupted, final number rolled is 0 
 
-    return {'play_interruption': play_interruption, 'player_position': player_position}  
+    return {'play_interruption': play_interruption, 'total_number_rolled': final_number}  
 
 
 
