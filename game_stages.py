8967408da7e3d6397bae42a9e6dd9c4fd973d52f
khushwant-learn <irahulguliani@gmail.com>
2024-09-03 05:24:52 +0530

    snakes and ladders both affecting the players

--- game_stages.py
+++ game_stages.py
@@ -25,8 +25,17 @@ def game_start():
         player_1_rolled_number = returned_dict_player_1['total_number_rolled']
         player_1_position += player_1_rolled_number
 
-        # Draw Board
-        draw_board_with_ladders_and_players(snakes=snakes, ladders=ladders, player_1_position=player_1_position, player_2_position=player_2_position)
+        # Informing the players about their  movement
+        # If rolled number is not zero ( Zero means number rolled is 6 , all the three times and the player's turn vanished)
+        if player_1_rolled_number > 0:
+            print(f" Player P1 moves {player_1_rolled_number} {'place' if player_1_rolled_number == 1 else 'places'} forward to box-number: {player_1_position}")
+        
+        # Update Player position if there is a ladder's foot or snake's head on the current player position 
+        updated_player_1_position = update_player_position(player_1_position, snakes=snakes, ladders=ladders)
+
+        # Draw Board with updated player_1 position
+        draw_board_with_ladders_and_players(snakes=snakes, ladders=ladders, player_1_position=updated_player_1_position, player_2_position=player_2_position)
+
 
         print(f"----------PLAYER_2-----------")
         returned_dict_player_2 = roll_dice()
@@ -37,18 +46,20 @@ def game_start():
         player_2_rolled_number = returned_dict_player_2['total_number_rolled']
         player_2_position += player_2_rolled_number
         
-        # Draw Board
-        draw_board_with_ladders_and_players(snakes=snakes, ladders=ladders, player_1_position=player_1_position, player_2_position=player_2_position)
+        if player_2_rolled_number > 0:
+            print(f" Player P2 moves {player_2_rolled_number} {'place' if player_2_rolled_number == 1 else 'places'} forward to box-number: {player_2_position}")
+
+        updated_player_2_position = update_player_position(player_2_position, snakes=snakes, ladders=ladders)
+
+        # Draw Board with both player_1 and player_2 updated position
+        draw_board_with_ladders_and_players(snakes=snakes, ladders=ladders, player_1_position=updated_player_1_position, player_2_position=updated_player_2_position)
 
 
-        # Update the player_position
-        # player_position += final_number
-    
-        if player_1_position >= 100:
+        if updated_player_1_position >= 100:
             game_finished('player_1')
             break
 
-        elif player_2_position >= 100:
+        elif updated_player_2_position >= 100:
             game_finished('player_2')
             break
 
@@ -125,3 +136,25 @@ def roll_a_dice_loop():
             print("Your Turn vanished!")
             break
     return final_number
+
+
+def update_player_position(player_position, snakes, ladders):
+
+            ladder_foots = [ladder[0] for ladder in ladders]    # first element of the each tuple in the list of tuples
+            ladder_heads = [ladder[1] for ladder in ladders]    # second element ...
+
+            snake_tails = [snake[0] for snake in snakes]
+            snake_heads = [snake[1] for snake in snakes]
+
+            if player_position in snake_heads:
+                player_updated_position = snake_tails[snake_heads.index(player_position)]   # tail at same index where head is
+                print(f"Your player is on a snake's head, so sorry to take you to its tail that is box-number: {player_updated_position}")
+
+            elif player_position in ladder_foots:
+                player_updated_position = ladder_heads[ladder_foots.index(player_position)]   # head at same index where foot is
+                print(f"your player is on a ladder's foot, happy to move it to its head that is box-number: {player_updated_position}")
+
+            else:
+                player_updated_position = player_position
+
+            return player_updated_position
